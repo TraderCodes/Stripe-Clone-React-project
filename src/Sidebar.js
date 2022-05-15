77a9d810fb41d39bed44e2d85197c2ec265f67b3
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-15 07:44:24 -0700

    🎉sidebar toggle option done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,47 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import sublinks from './data';
+import { useGlobalContext } from './context';
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+   const { isSidebarOpen, closeSidebar } = useGlobalContext();
+   return (
+      <aside
+         className={isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'}
+      >
+         <div className="sidebar">
+            {/* sidebar close button */}
+            <button className="close-btn" onClick={closeSidebar}>
+               <FaTimes />
+            </button>
+            <div className="sidebar-links">
+               {/* map through sidebar data */}
+               {sublinks.map((item, index) => {
+                  const { links, page } = item;
+                  return (
+                     <article key={index}>
+                        <h4>{page}</h4>
 
-export default Sidebar
+                        {/* for each page map inside each links  */}
+                        <div className="sidebar-sublinks">
+                           {links.map((link, index) => {
+                              const { url, icon, label } = link;
+                              return (
+                                 <a href={url} key={index}>
+                                    {/* for eachitem that map through we return */}
+                                    {icon}
+                                    {label}
+                                 </a>
+                              );
+                           })}
+                        </div>
+                     </article>
+                  );
+               })}
+            </div>
+         </div>
+      </aside>
+   );
+};
+
+export default Sidebar;
